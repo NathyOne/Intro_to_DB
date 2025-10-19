76cdd06650a66ae1e9c801cc247e939006040878
NathyOne <nathanielabebe32@gmail.com>
2025-10-19 09:08:56 +0300

    creating tables

--- task_2.sql
+++ task_2.sql
@@ -21,7 +21,7 @@ CREATE TABLE Customers (
 
 CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
-    customer_id BIGINT,
+    customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
 );
