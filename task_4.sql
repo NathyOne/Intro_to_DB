425909eeea4546bbe1bb3a0af1eb31d936f81859
NathyOne <nathanielabebe32@gmail.com>
2025-10-19 09:28:01 +0300

    secribing a table

---
+++ task_4.sql
@@ -0,0 +1,16 @@
+SELECT
+    COLUMN_NAME AS Field,
+    COLUMN_TYPE AS Type,
+    IF(
+        IS_NULLABLE = 'NO',
+        'NO',
+        'YES'
+    ) AS Null,
+    COLUMN_KEY AS Key,
+    COLUMN_DEFAULT AS Default,
+    EXTRA AS Extra
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = DATABASE()
+    AND TABLE_NAME = 'books'
+ORDER BY ORDINAL_POSITION;
